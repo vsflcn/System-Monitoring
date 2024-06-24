ad07968696064b53a72d0e7741e5448a6517b061
vsflcn <falcoooneee@proton.me>
2024-06-24 16:42:15 +0200

    added endless while loop and variable to collect CPU's usage data using top command

--- data.sh
+++ data.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 #This script will monitor data about CPU and memory usage on Linux Ubuntu VM and collect it in the all_data.log file
 
+while :
+do
+    #Current CPU & memory usage state
+    cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}') 
