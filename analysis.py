8de2aafa8e6d85f8800caceb1e6f1c17c53c1d71
vsflcn <falcoooneee@proton.me>
2024-06-24 19:06:06 +0200

    added feature no authentication for disposable email

--- analysis.py
+++ analysis.py
@@ -6,6 +6,10 @@ from dotenv import load_dotenv
 #Load environment variables
 load_dotenv()
 
+# No authentication for disposable email
+SMTP_USER = None
+SMTP_PASSWORD = None
+
 # Thresholds
 CPU_THRESHOLD = 80.0  # in percentage
 MEMORY_THRESHOLD = 3024  # in MB
