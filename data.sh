d94f06209cda214d653bc0b7830bb21ee451f260
vsflcn <falcoooneee@proton.me>
2024-06-24 16:48:01 +0200

    added comment about how free command works

--- data.sh
+++ data.sh
@@ -5,4 +5,4 @@ while :
 do
     #Current CPU & memory usage state
     cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}') # top command provides dynamic state of working OS in real time 
-    memUsage=$(free -m | awk '/Mem/{print $3}')
+    memUsage=$(free -m | awk '/Mem/{print $3}') #free command shows total amount of free and used physical and swap memory on the system
