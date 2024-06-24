febf89c251f6d06d2a5e22a50347f56269dcc8e8
vsflcn <falcoooneee@proton.me>
2024-06-24 16:49:36 +0200

    added print statements

--- data.sh
+++ data.sh
@@ -6,3 +6,9 @@ do
     #Current CPU & memory usage state
     cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}') # top command provides dynamic state of working OS in real time 
     memUsage=$(free -m | awk '/Mem/{print $3}') #free command shows total amount of free and used physical and swap memory on the system
+
+    #Print the usage
+    echo "Cpu Usage: $cpuUsage%"
+    echo "Memory usage: $memUsage MB"
+
+    
